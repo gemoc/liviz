1154f18af948803ebcbaff5500969acf669431a3
Skinz3 <leafburst60@gmail.com>
2021-07-23 16:23:54 +0200

    Add tooltip

--- Visualizer/js/plot/points.js
+++ Visualizer/js/plot/points.js
@@ -44,7 +44,7 @@ Points.prototype.initialize = function ()
 
 
     this.addLegend();
-    this.addToolTip();
+    this.addToolTip(margin, width);
 
     this.svg.append("defs").append("clipPath")
         .attr("id", "clip")
@@ -111,34 +111,22 @@ Points.prototype.append = function (variableName, obj)
 }
 
 
-
-
-
-Points.prototype.addToolTip = function ()
+Points.prototype.addToolTip = function (margin, width)
 {
-    const margin = { top: 50, right: 30, bottom: 30, left: 60 },
-        width = document.getElementById("container").offsetWidth * 0.95 - margin.left - margin.right,
-        height = 400 - margin.top - margin.bottom;
-
-
     var div = d3.select("body").append("div")
         .attr("class", "tooltip")
         .attr("x", width - 300)
         .attr("y", 0)
         .style("opacity", 0);
 
-    var verticalLine = this.svg.append("line")
-        .attr("class", "verticalLine")
-        .attr("x1", 0)
-        .attr("y1", 0)
-        .attr("x2", 0)
-        .attr("y2", height)
-        .style("opacity", 0);
+
+    var svg = this.svg;
 
 
-    var x = this.x;
-    var y = this.y;
-    var data = this.data;
+
+
+
+
 
     d3.select("#container").on("mousemove", function ()
     {
@@ -146,49 +134,43 @@ Points.prototype.addToolTip = function ()
         var mouse_x = d3.mouse(this)[0];
         var mouse_y = d3.mouse(this)[1];
 
+
         // Si la position de la souris est en dehors de la zone du graphique, on arrête le traitement
         if (mouse_x < margin.left || mouse_x > (width + margin.left) || mouse_y < margin.top || mouse_y > (400 - margin.bottom))
         {
             return;
         }
 
-        // Grâce à la fonction 'invert' nous récupérons la date correspondant à notre position
-        // A noter, il faut soustraire la marge à gauche pour que la valeur soit correct.
-        var selected = x.invert(mouse_x - margin.left);
+        var circleSet = svg.selectAll("circle");
 
-        // Positionnement de la barre verticale toujours en tenant compte de la marge
-        verticalLine.attr("x1", mouse_x - margin.left);
-        verticalLine.attr("x2", mouse_x - margin.left);
-        verticalLine.style("opacity", 1);
-
-        // Le revert est précis à la milliseconde, ce qui n'est pas le cas de nos données
-
-        var entry = data.get("var2");
-
-        if (typeof entry === "undefined")
+        circleSet.on("mouseover", function (obj, indice, it)
         {
-            return;
-        }
-        // Si une entrée existe pour la date sélectionnée nous pouvons afficher les données.
+            console.log(obj);
 
-        // Le comportement est équivalent aux précédents exemples pour le tooltip.
-        div.style("opacity", .9);
-        div.style("left", (d3.event.pageX + 30) + "px")
-            .style("top", (d3.event.pageY - 60) + "px")
-            .html("<b>x : </b>" + selected + "<br>"
-                + "<b>maVariable : </b>Work in progress<br>"
-                + "<b>var2 : </b>Work in progress<br>");
-    }).on("mouseout", function ()
-    {
-        var mouse_x = d3.mouse(this)[0];
-        var mouse_y = d3.mouse(this)[1];
+            div.style("opacity", .9);
+            div.style("left", (d3.event.pageX + 30) + "px")
+                .style("top", (d3.event.pageY - 60) + "px")
+                .html("<b>x : </b>" + obj.date + "<br>"
+                    + "<b>y : </b>" + obj.close + "<br>"
+                    + "<b>indice : </b>" + indice + "<br>");
 
-        // Si la position de la souris est en dehors de la zone du graphique, on masque la ligne et le tooltip
-        if (mouse_x < margin.left || mouse_x > (width + margin.left) || mouse_y < margin.top || mouse_y > (400 - margin.bottom))
+
+        }).on("mouseout", function ()
         {
-            div.style("opacity", 0);
-            verticalLine.style("opacity", 0);
-        }
+            var mouse_x = d3.mouse(this)[0];
+            var mouse_y = d3.mouse(this)[1];
+
+            // Si la position de la souris est en dehors de la zone du graphique, on masque la ligne et le tooltip
+            if (mouse_x < margin.left || mouse_x > (width + margin.left) || mouse_y < margin.top || mouse_y > (400 - margin.bottom))
+            {
+                div.style("opacity", 0);
+
+            }
+        })
+
+
+
+
     });
 }
 
