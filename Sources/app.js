c86f6c99d477ab05f782a520dd8a3483a437eadf
Skinz3 <leafburst60@gmail.com>
2021-08-09 15:33:33 +0200

    Add misc files

--- Sources/app.js
+++ Sources/app.js
@@ -1,3 +1,5 @@
+const port = 3000;
+
 var path = require('path');
 var express = require("express");
 var app = express();
@@ -6,9 +8,9 @@ var htmlPath = path.join(__dirname, 'html');
 
 app.use(express.static(htmlPath));
 
-app.listen(3000, () =>
+app.listen(port, () =>
 {
-    console.log("Server running on port 3000");
+    console.log("Server running on port " + port);
 
     app.get("/url", (req, res, next) =>
     {
